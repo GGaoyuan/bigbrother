be7da07ad013e40016e22cb7062e1956533a2041
gaoyuan18 <yuangao18@iflytek.com>
2024-11-25 17:23:57 +0800

    add

--- client_web/src/App.vue
+++ client_web/src/App.vue
@@ -1,30 +1,33 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import VerticalMenu from "./components/VerticalMenu.vue";
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="vertical_menu">
+      <VerticalMenu/>
+    </div>
+    <div class="content_area">
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.container {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.vertical_menu {
+  background-color: #f0f0f0;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.content_area {
+  flex-grow: 1;
+  background-color: #998922;
+  /*overflow-y: auto;  如果内容超出，显示滚动条 */
 }
 </style>
--- client_web/src/main.js
+++ client_web/src/main.js
@@ -1,5 +1,4 @@
 // import { createApp } from 'vue'
-// import './style.css'
 // import App from './App.vue'
 //
 // createApp(App).mount('#app')
@@ -8,8 +7,8 @@
 
 import { createApp } from 'vue';
 import Antd from 'ant-design-vue';
-import './style.css'
 import App from './App.vue'
+import './style.css'
 
 const app = createApp(App);
 
