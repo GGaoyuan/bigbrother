b5b9f1d6de7bf71476984827552717fa05f8b4d7
gaoyuan18 <yuangao18@iflytek.com>
2024-11-25 14:40:51 +0800

    add

---
+++ web/bigbrother/src/components/VerticalMenu.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+export default {
+    
+}
+</script>
+
+
+
+<template>
+    <div>
+        <h1 class="green">{{ '123123123' }}</h1>
+    </div>
+</template>
+
+
+
+
+<style scoped>
+    
+
+
+</style>
