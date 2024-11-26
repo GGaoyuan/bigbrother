85bfe702899e5ce2a296479f01bae8aeddf75b3f
gaoyuan18 <yuangao18@iflytek.com>
2024-11-26 20:11:14 +0800

    add

--- server/app/util/file.py
+++ server/app/util/file.py
@@ -1,13 +1,14 @@
 
 class File:
-    def __init__(self, foder: str, name: str):
+    def __init__(self, foder_path: str, name: str):
         """
         获取完整路径
         """
-        if not all(isinstance(item, str) for item in folder_link):
-            raise ValueError("列表中的所有元素必须是字符串")
-        path = os.path.join(Path(__file__).parent.absolute().parent.parent, 'static')
-        for folder in folder_link:
-            path = os.path.join(path, folder)
-        self.__path = path
-        self.__make_dir()
+        pass
+        # if not all(isinstance(item, str) for item in folder_link):
+        #     raise ValueError("列表中的所有元素必须是字符串")
+        # path = os.path.join(Path(__file__).parent.absolute().parent.parent, 'static')
+        # for folder in folder_link:
+        #     path = os.path.join(path, folder)
+        # self.__path = path
+        # self.__make_dir()
--- server/app/util/folder.py
+++ server/app/util/folder.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import os
 
 class Folder:
     def __init__(self, folder_link: list[str]):
@@ -10,7 +9,7 @@ class Folder:
             raise ValueError("列表中的所有元素必须是字符串")
         path = Path.joinpath(Path(__file__).parent.absolute().parent.parent, 'static')
         for folder in folder_link:
-            path = os.path.join(path, folder)
+            path = Path.joinpath(path, folder)
         self.__path = path
         self.__make_dir()
 
@@ -18,24 +17,11 @@ class Folder:
         """
         创建文件夹
         """
-        if len(self.__path) == 0:
-            raise ValueError(f'path is none')
-        if not os.path.exists(self.__path):
-            os.makedirs(self.__path)
+        path = Path(self.__path)
+        if not path.exists():
+            path.mkdir(parents=True, exist_ok=True)
         else:
             print(f'{self.__path} already exists')
-
-    def file_exist(self, file_name: str) -> bool:
-        """
-        文件夹下的文件是否存在
-        :param file_name:
-        :return:
-        """
-        file_path = os.path.join(self.__path, file_name)
-        if not os.path.exists(file_path):
-            return True
-        else:
-            return False
 
     def path(self) -> str:
         return self.__path
