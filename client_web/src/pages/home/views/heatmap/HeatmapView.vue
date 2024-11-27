ba8e85dbc2d852f796856cb6867c10ec3d5c12db
gaoyuan18 <yuangao18@iflytek.com>
2024-11-27 18:33:27 +0800

    add

--- client_web/src/pages/home/views/heatmap/HeatmapView.vue
+++ client_web/src/pages/home/views/heatmap/HeatmapView.vue
@@ -1,17 +1,29 @@
-<script setup>
-
-</script>
-
 <template>
-  <div class="container">
-
-  </div>
+  <div ref="heatmapChart" style="width: 600px; height: 400px;"></div>
 </template>
 
-<style scoped>
-.container {
-  background-color: #FFFAAA;
-  display: flex;
-  height: 100vh;
-}
+<script setup>
+import { onMounted, ref } from 'vue';
+import Plotly from 'plotly.js-dist';
+
+const heatmapChart = ref(null);
+
+const data = [
+  {
+    z: [[1, null, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
+    x: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    y: ['Morning', 'Afternoon', 'Evening'],
+    type: 'heatmap',
+    hoverongaps: false
+  }
+];
+
+
+onMounted(() => {
+  Plotly.newPlot(heatmapChart.value, data);
+});
+</script>
+
+<style>
+/* 可以在这里添加一些样式 */
 </style>
