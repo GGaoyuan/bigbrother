48d11f0c5d580c7f7ef92dbfc01ae65c328d546e
gaoyuan18 <yuangao18@iflytek.com>
2024-11-29 19:14:08 +0800

    add

--- server/run.py
+++ server/run.py
@@ -1,7 +1,10 @@
-from app import create_app
+from flask import Flask
 from flask_cors import CORS
+from app.routes import main_route, test_route
 
-app = create_app()
+app = Flask(__name__)
+app.register_blueprint(main_route.bp)
+app.register_blueprint(test_route.bp)
 # 允许所有源访问，前端报错has been blocked by CORS policy: No 'Access-Control-Allow-Origin
 CORS(app, resources={r"/*": {"origins": "*"}})
 
