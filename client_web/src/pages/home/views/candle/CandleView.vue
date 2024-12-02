c22ba88344462cc00b449bddd68b863b6b3eeb46
gaoyuan18 <yuangao18@iflytek.com>
2024-12-02 23:41:13 +0800

    add

--- client_web/src/pages/home/views/candle/CandleView.vue
+++ client_web/src/pages/home/views/candle/CandleView.vue
@@ -1,12 +1,12 @@
 <script setup>
-import HeatmapComponent from "../../../../components/HeatmapComponent.vue";
-import {nextTick, onMounted, ref} from "vue";
+import { ref, onMounted, onUnmounted, nextTick } from 'vue';
+import * as echarts from 'echarts';
 
+// 创建一个响应式引用来保存DOM元素
+const chartDom = ref(null);
 const heatmapData = ref(null);
-// const hmData = ref(null);
-const hm1 = ref(null);
-const hm2 = ref(null);
-const hm3 = ref(null);
+
+let chartInstance = null;
 
 function fetchHeatmapData() {
   fetch('http://127.0.0.1:5000/industry/heatmap')
@@ -17,39 +17,105 @@ function fetchHeatmapData() {
         return response.json();
       })
       .then((json) => {
-        // heatmapData.value = '1111'
         heatmapData.value = json.data;
-        console.log('json response');
-        // console.log(heatmapData.value.data)
-        // buildHeatmap()
+        console.log(heatmapData.value.data)
+        buildHeatmap()
       })
       .catch((error) => {
         console.error(error);
       })
 }
 
+function buildHeatmap() {
+  chartInstance = echarts.init(chartDom.value)
+  console.log(heatmapData.value.title)
+  const option = {
+
+    title: {
+      text: heatmapData.value.title
+    },
+    tooltip: {
+      position: 'top'
+    },
+    grid: {
+      height: '50%',
+      top: '10%'
+    },
+    xAxis: {
+      type: 'category',
+      data: heatmapData.value.xAxis,
+      splitArea: {
+        show: true
+      }
+    },
+    yAxis: {
+      type: 'category',
+      data: heatmapData.value.yAxis,
+      splitArea: {
+        show: true
+      }
+    },
+    visualMap: {
+      min: -5,
+      max: 5,
+      calculable: true,
+      orient: 'horizontal',
+      left: 'center',
+      bottom: '0px',
+      inRange: {
+        // 在范围内的颜色
+        color: ['#50a3ba', '#ffffff' ,'#d94e5d'] // 颜色渐变
+      },
+    },
+    series: [
+      {
+        name: 'Punch Card\n222\n33333333\naaaaaaaaaa\n00000000000000',
+        type: 'heatmap',
+        data: heatmapData.value.data,
+        label: {
+          show: true
+        },
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+  chartInstance.setOption(option);
+}
+
+// 初始化ECharts实例并设置配置项（这里以折线图为例，但可灵活替换）
 onMounted(async () => {
   await nextTick(); // 确保DOM已经渲染完成
   fetchHeatmapData()
 });
 
 
+
+// 销毁ECharts实例
+onUnmounted(() => {
+  if (chartInstance != null && chartInstance.dispose) {
+    chartInstance.dispose();
+  }
+  if (chartInstance == null) {
+    console.log('chartInstance 已销毁')
+  } else {
+    console.log('chartInstance 未销毁')
+  }
+  //flex-direction: column
+});
 </script>
 
+
 <template>
-  <div class="container">
-    <HeatmapComponent :heatmapData="heatmapData" ref="hm1" style="background-color: #880022"></HeatmapComponent>
-<!--    <HeatmapComponent :heatmapData="heatmapData" ref="hm2" style="background-color: #f9f9f9"></HeatmapComponent>-->
-<!--    <HeatmapComponent :heatmapData="heatmapData" ref="hm3" style="background-color: #FF8AA2"></HeatmapComponent>-->
-  </div>
+  <div ref="chartDom" style="width: 100%; height: auto;"></div>
 </template>
 
 
 <style scoped>
-.container {
-  background-color: #646cff;
-  display: flex;
-  flex-direction: column;
-  width: 100vw;
-}
+
+/* 添加一些CSS样式来美化图表容器（可选） */
 </style>
