e49520f09b6e0d249ed9d554b3e112b81dfd3611
gaoyuan18 <yuangao18@iflytek.com>
2024-12-11 19:45:11 +0800

    add

--- server/app/stock/service/market.py
+++ server/app/stock/service/market.py
@@ -1,4 +1,5 @@
 from ..dao.ranking import *
+from ..service import *
 def get_hot_by_rank(start_date: str, end_date: str):
     """
     获取龙虎榜和相关的数据
@@ -6,6 +7,5 @@ def get_hot_by_rank(start_date: str, end_date: str):
     # 获取龙虎榜的数据列表
     # 判断龙虎榜的数据归属
     # 我是看资金流入量打板的，在10点20分左右监测到有大资
-
     leader_list = get_leader_list(start_date, end_date)
     print(leader_list)
