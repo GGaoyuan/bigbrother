fdb172569fb42fd5b252e9ccbf092be24ff899f6
yuangao18 <yuangao18@iflytek.com>
2025-12-01 11:05:33 +0800

    web front

--- bigbrother-web-front/src/App.vue
+++ bigbrother-web-front/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import NavigatorBar from "@/components/NavigatorBar.vue";
 </script>
 
 <template>
+  <NavigatorBar></NavigatorBar>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
