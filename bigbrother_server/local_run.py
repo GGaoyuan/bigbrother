f27cbbeb3d12b281538f983278733a2998f4a760
yuangao18 <yuangao18@iflytek.com>
2026-02-10 11:16:50 +0800

    add

--- bigbrother_server/local_run.py
+++ bigbrother_server/local_run.py
@@ -1,11 +1,23 @@
 import akshare as ak
 import akcopy
 
-if __name__ == '__main__':
-    print("11111")
-    stock_board_industry_summary_ths_df = ak.stock_board_industry_name_ths()
-    print(stock_board_industry_summary_ths_df)
 
-    stock_board_concept_index_ths_df = akcopy.stock_board_concept_name_ths()
-    print(stock_board_concept_index_ths_df)
-    print("222222")
+
+def slaveStrategy():
+    ETFCodeList = [
+        563020,
+    ]
+    pass
+
+if __name__ == '__main__':
+    # print("11111")
+    # stock_board_industry_summary_ths_df = ak.stock_board_industry_name_ths()
+    # print(stock_board_industry_summary_ths_df)
+    #
+    # stock_board_concept_index_ths_df = akcopy.stock_board_concept_name_ths()
+    # print(stock_board_concept_index_ths_df)
+    # print("222222")
+    slaveStrategy()
+
+
+
